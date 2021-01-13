47f6b92dde7d8123f467a88e6e5c063253baf449
FrensysThomas87 <delta873@gmail.com>
2021-01-13 17:54:02 +0100

    Implementata la funzionalità che mi fa tornare indietro e aggiunti i commenti

--- main.js
+++ main.js
@@ -1,46 +1,41 @@
 $(document).ready(function(){
 
-
-
-
-
   $('.slider-wrapper .next .fas.fa-angle-right').click(function(){
+    //Recupero l'immagine dove mi trovo al momento
     var currentImage = $('.images img.active');
+
+    //Recupero la prima immagine
     var firstImage = $('.images img.first');
-    // currentImage++;
 
-
-    // if(currentImage === 4){
-    //   $('img.first').addClass('active');
-    //   currentImage = 0;
-    // }
-
+    // Se l'immagine corrente ha la classe "last"
     if(currentImage.hasClass('last')){
+
+      // alla prima immagine do la classe active
       firstImage.addClass('active');
+
+      //all'immagine corrente tolgo la classe active
       currentImage.removeClass('active');
     }else {
+
+      //vai avanti normalmente
       currentImage.removeClass('active').next().addClass('active');
     }
 
-    console.log(currentImage);
-
-});
-
+  });
 
   $('.slider-wrapper .prev .fas.fa-angle-left').click(function(){
-    $('img.active').removeClass('active').prev().addClass('active');
+    // Qui sotto faccio il contrario
 
+    var currentImage = $('.images img.active');
+    var lastImage = $('.images img.last');
 
-    if(currentImage === 0){
-      $('img.last').addClass('active');
-      currentImage++;
-
+    if(currentImage.hasClass('first')){
+      lastImage.addClass('active');
+      currentImage.removeClass('active');
+    }else{
+      currentImage.removeClass('active').prev().addClass('active');
     }
 
-    console.log(currentImage);
-
 });
-
-
 
 });
