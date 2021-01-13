2d93f473c710eb8507694d17bc974e1a801e03d0
FrensysThomas87 <delta873@gmail.com>
2021-01-13 16:28:50 +0100

    Implementata funzione che torna alla prima slide superata l'ultima

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 $(document).ready(function(){
+
+  var imgLast = $('.last.active');
+  var currentImage = 0;
+
+  console.log($('img'));
+
   $('.slider-wrapper .next .fas.fa-angle-right').click(function(){
     $('img.active').removeClass('active').next().addClass('active');
-  });
+    currentImage++;
+    console.log(currentImage);
+
+    if(currentImage === 4){
+      $('img.first').addClass('active');
+      currentImage = 0;
+    }
+
+});
+
+$('.slider-wrapper .next .fas.fa-angle-right').click(function(){
+
+
+});
+
+
+  // $('.slider-wrapper .next .fas.fa-angle-right').click(function(){
+  //   $('img.last').removeClass('active').prev('.first').addClass('active');
+  // });
 
   $('.slider-wrapper .prev .fas.fa-angle-left').click(function(){
     $('img.active').removeClass('active').prev().addClass('active');
   });
+
+
 
 });
