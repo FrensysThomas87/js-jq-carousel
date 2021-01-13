4d7c85d5b748807b76a58078adc2f2b59f5f4750
FrensysThomas87 <delta873@gmail.com>
2021-01-13 18:12:23 +0100

    Implementata funzionalità che colora i pallini in base alla slide selezionata

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   $('.slider-wrapper .next .fas.fa-angle-right').click(function(){
     //Recupero l'immagine dove mi trovo al momento
     var currentImage = $('.images img.active');
+    var currentDot = $('.nav i.active');
+    var firsDot = $('.nav i.first');
 
     //Recupero la prima immagine
     var firstImage = $('.images img.first');
@@ -21,6 +23,13 @@ $(document).ready(function(){
       currentImage.removeClass('active').next().addClass('active');
     }
 
+    if(currentDot.hasClass('last')){
+      firsDot.addClass('active');
+      currentDot.removeClass('active');
+    }else{
+      currentDot.removeClass('active').next().addClass('active');
+    }
+
   });
 
   $('.slider-wrapper .prev .fas.fa-angle-left').click(function(){
@@ -28,6 +37,8 @@ $(document).ready(function(){
 
     var currentImage = $('.images img.active');
     var lastImage = $('.images img.last');
+    var currentDot = $('.nav i.active');
+    var lastDot = $('.nav i.last');
 
     if(currentImage.hasClass('first')){
       lastImage.addClass('active');
@@ -35,6 +46,15 @@ $(document).ready(function(){
     }else{
       currentImage.removeClass('active').prev().addClass('active');
     }
+
+    if(currentDot.hasClass('first')){
+      lastDot.addClass('active');
+      currentDot.removeClass('active');
+    }else{
+      currentDot.removeClass('active').prev().addClass('active');
+    }
+
+
 
 });
 
