00382db237c4a4cad2abb033a015983a1660de0a
FrensysThomas87 <delta873@gmail.com>
2021-01-13 17:03:12 +0100

    Ultime modifiche

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
 $(document).ready(function(){
 
-  var imgLast = $('.last.active');
+
   var currentImage = 0;
 
-  console.log($('img'));
+
 
   $('.slider-wrapper .next .fas.fa-angle-right').click(function(){
     $('img.active').removeClass('active').next().addClass('active');
     currentImage++;
-    console.log(currentImage);
+
 
     if(currentImage === 4){
       $('img.first').addClass('active');
       currentImage = 0;
     }
 
-});
-
-$('.slider-wrapper .next .fas.fa-angle-right').click(function(){
-
+    console.log(currentImage);
 
 });
 
-
-  // $('.slider-wrapper .next .fas.fa-angle-right').click(function(){
-  //   $('img.last').removeClass('active').prev('.first').addClass('active');
-  // });
 
   $('.slider-wrapper .prev .fas.fa-angle-left').click(function(){
     $('img.active').removeClass('active').prev().addClass('active');
-  });
+
+
+    if(currentImage === 0){
+      $('img.last').addClass('active');
+      currentImage++;
+
+    }
+
+    console.log(currentImage);
+
+});
 
 
 
